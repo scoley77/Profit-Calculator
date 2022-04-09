a2f0850f57e2c253b5a182a11c3e4c59a3748cad
Sara Coley <79733957+scoley77@users.noreply.github.com>
2022-04-09 02:34:09 +0000

    added additional product

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ import classes
 
 products = []
 
-products.append( classes.Product('Choco Pins', 1.10, 5.00, 14) )
-products.append( classes.Product('Nerikiri Pets', 3.80, 12.00, 12) )
+products.append( classes.Product('Choco Pins', 1.10, 7.00, 14) )
+products.append( classes.Product('Nerikiri Pets', 3.80, 15.00, 12) )
+products.append( classes.Product('Mini Teapots', 4.50, 15.00, 5) )
 products.append( classes.Product('5x7 Prints', 0.70, 8.00, 20) )
 products.append( classes.Product('11x17 Prints', 1.00, 15.00, 10) )
+products.append( classes.Product('Big Fat Potion', 2.50, 10.00, 7) )
 products.append( classes.Product('Big Potion', 1.50, 7.00, 7) )
 products.append( classes.Product('Little Potion', 0.75, 5.00, 13) )
 products.append( classes.Product('Bottled Clay Pins', 1.75, 10.00, 6) )
